848f45e1d147bb71ba732a7d1d517cab6412c001
chohnna <chohnna@gmail.com>
2024-10-07 13:15:54 -0400

    src, script update

--- src/script.py
+++ src/script.py
@@ -1,12 +1,22 @@
-# create instance
-from water import water
+# Create an instance
+from water_geo import water
 from src import Pyscf_helper
 
 molecule = water
 spin = 0
 basis_set = 'sto-3g'
 
+# Initialize Pyscf_helper
 scf_water_init = Pyscf_helper()
+
+# Run SCF calculation
 scf_water = scf_water_init.pyscf_scf(molecule, spin, basis_set)
 
-cis_water = scf_water_init.configuration_interaction_singles(mean_field ,1, 1)
+# Run configuration interaction singles
+n_singlets = 1  # Example value for number of singlets
+n_triplets = 1  # Example value for number of triplets
+cis_singlet_E, density_singlet, cis_triplet_E, density_triplet = scf_water_init.configuration_interaction_singles(n_singlets, n_triplets)
+
+# Print results
+print("CIS Singlet Energy:", cis_singlet_E)
+print("CIS Triplet Energy:", cis_triplet_E)
--- src/src.py
+++ src/src.py
@@ -5,44 +5,61 @@ import matplotlib.pyplot as plt
 import numpy as np
 import json
 
-class Pyscf_helper:
-    # constructers
-    def __init__(self):
-        # instance variables
-        self.mf  = None
-        self.mol = None
-        self.h = None
-        self.g      = None
-        self.n_orb  = None
-        self.C      = None
-        self.S      = None
-        self.J      = None
-        self.K      = None
-        self.F = None
 
-    # methods
-        
-    def tda_denisty_matrix(td, state_id):
-    # ‘’'
-    # Taking the TDA amplitudes as the CIS coefficients, calculate the density
-    # matrix (in AO basis) of the excited states
-    # ‘’'
+
+class active_space:
+    def __init__(self):
+        self.mf = None
+
+    def tda_density_matrix(self, td, state_id):
+        """
+        Taking the TDA amplitudes as the CIS coefficients, calculate the density
+        matrix (in AO basis) of the excited states.
+        """
         cis_t1 = td.xy[state_id][0]
-        dm_oo =-np.einsum('ia,ka->ik', cis_t1.conj(), cis_t1)
+        dm_oo = -np.einsum('ia,ka->ik', cis_t1.conj(), cis_t1)
         dm_vv = np.einsum('ia,ic->ac', cis_t1, cis_t1.conj())
         # The ground state density matrix in mo_basis
         mf = td._scf
         dm = np.diag(mf.mo_occ)
         # Add CIS contribution
         nocc = cis_t1.shape[0]
-        # Note that dm_oo and dm_vv correspond to spin-up contribution. “*2” to
+        # Note that dm_oo and dm_vv correspond to spin-up contribution. "*2" to
         # include the spin-down contribution
-        dm[:nocc,:nocc] += dm_oo * 2
-        dm[nocc:,nocc:] += dm_vv * 2
+        dm[:nocc, :nocc] += dm_oo * 2
+        dm[nocc:, nocc:] += dm_vv * 2
         # Transform density matrix to AO basis
         mo = mf.mo_coeff
         dm = np.einsum('pi,ij,qj->pq', mo, dm, mo.conj())
         return dm
+    
+    def get_natural_orbital():
+
+    def calculate_in_acitve_space():
+
+    def Embed_mean_field():
+
+
+
+    
+    
+
+class Pyscf_helper:
+    # Constructor
+    def __init__(self):
+        # Instance variables
+        self.mf = None
+        self.mol = None
+        self.h = None
+        self.g = None
+        self.n_orb = None
+        self.C = None
+        self.S = None
+        self.J = None
+        self.K = None
+        self.F = None
+
+    # Methods
 
     def pyscf_scf(self, molecule, spin, basis_set):
         mol = gto.Mole()
@@ -51,10 +68,10 @@ class Pyscf_helper:
         mol.spin = spin
         mol.build()
 
-        mean_field = scf.RHF(mol).run(verbose = 4)
+        mean_field = scf.RHF(mol).run(verbose=4)
         mean_field.analyze()
 
-        core_hamiltornian = mean_field.get_hcore()
+        core_hamiltonian = mean_field.get_hcore()
         mo_energy = mean_field.mo_energy
         mo_occ = mean_field.get_occ(mo_energy)
         overlap = mean_field.get_ovlp()
@@ -62,23 +79,31 @@ class Pyscf_helper:
         fock_matrix = mean_field.get_fock()
         density = mean_field.make_rdm1()
         
-        return core_hamiltornian, mo_energy, mo_occ, overlap, coeff, fock_matrix, density
+        self.mf = mean_field
+
+        return core_hamiltonian, mo_energy, mo_occ, overlap, coeff, fock_matrix, density
     
-    def configuration_interaction_singles(Pyscf_helper, n_singlets, n_triplets):
-        # compute singlets
-        mytd = tdscf.TDA(mean_field)
-        mytd.singlet = True
-        mytd = mytd.run(nstates=n_singlets)
-        mytd.analyze()
-        cis_singlet_E = min(mytd.kernel()[0])
-        for i in range(mytd.nroots):
-            density += tda_denisty_matrix(mytd, i)
-        # compute triplets
-        mytd = tdscf.TDA(mean_field)
-        mytd.singlet = False
-        mytd = mytd.run(nstates=n_triplets)
-        mytd.analyze()
-        cis_triplet_E = min(mytd.kernel()[0])
-        for i in range(mytd.nroots):
-            D += tda_denisty_matrix(mytd, i)
+    def configuration_interaction_singles(self, n_singlets, n_triplets):
+        density_singlet = 0
+        density_triplet = 0
+
+        # Compute singlets
+        mytd_singlet = tdscf.TDA(self.mf)
+        mytd_singlet.singlet = True
+        mytd_singlet = mytd_singlet.run(nstates=n_singlets)
+        mytd_singlet.analyze()
+        cis_singlet_E = min(mytd_singlet.kernel()[0])
+        for i in range(mytd_singlet.nroots):
+            density_singlet += self.tda_density_matrix(mytd_singlet, i)
+        
+        # Compute triplets
+        mytd_triplet = tdscf.TDA(self.mf)
+        mytd_triplet.singlet = False
+        mytd_triplet = mytd_triplet.run(nstates=n_triplets)
+        mytd_triplet.analyze()
+        cis_triplet_E = min(mytd_triplet.kernel()[0])
+        for i in range(mytd_triplet.nroots):
+            density_triplet += self.tda_density_matrix(mytd_triplet, i)
+        
+        return cis_singlet_E, density_singlet, cis_triplet_E, density_triplet
 
